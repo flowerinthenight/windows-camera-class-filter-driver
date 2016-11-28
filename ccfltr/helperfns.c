4361db322b138e474590e83b13070d7a56afa5ad
Chew Esmero <chew.esmero@gmail.com>
2016-11-29 00:13:32 +0900

    Spacing.

--- ccfltr/helperfns.c
+++ ccfltr/helperfns.c
@@ -7,18 +7,18 @@
 
 void PrintGuidValues(PCHAR pszFn, PWCHAR pszLabel, LPGUID lpGuid)
 {
-	L("[%s] %S: %08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x\n",
-		pszFn,
-		pszLabel,
-		lpGuid->Data1,
-		lpGuid->Data2,
-		lpGuid->Data3,
-		lpGuid->Data4[0],
-		lpGuid->Data4[1],
-		lpGuid->Data4[2],
-		lpGuid->Data4[3],
-		lpGuid->Data4[4],
-		lpGuid->Data4[5],
-		lpGuid->Data4[6],
-		lpGuid->Data4[7]);
+    L("[%s] %S: %08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x\n",
+        pszFn,
+        pszLabel,
+        lpGuid->Data1,
+        lpGuid->Data2,
+        lpGuid->Data3,
+        lpGuid->Data4[0],
+        lpGuid->Data4[1],
+        lpGuid->Data4[2],
+        lpGuid->Data4[3],
+        lpGuid->Data4[4],
+        lpGuid->Data4[5],
+        lpGuid->Data4[6],
+        lpGuid->Data4[7]);
 }
